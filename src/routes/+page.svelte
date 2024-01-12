2ac6880f055ad8cbd8bb712ef24769fca58c4e24
wduf <wduf02@gmail.com>
2024-01-12 18:49:08 -0500

    skeleton project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- HTML. -->
+<main>
+	<h1 class='title'>title</h1>
+	<button class='button'>button</button>
+</main>
+
+<!-- SCSS. -->
+<style lang='scss'>
+	// Main styles.
+	@import '../scss/style.scss';
+</style>
+
+<!-- TypeScript. -->
+<script lang='ts'></script>
---
+++ src/routes/_template.svelte
@@ -0,0 +1,8 @@
+<!-- HTML. -->
+<main></main>
+
+<!-- CSS. -->
+<style lang='scss'></style>
+
+<!-- TypeScript. -->
+<script lang='ts'></script>
