caa4b961392c88c62a7852a0256e867d6ca54b2e
Mattheus Faria <mattheusfaria27@gmail.com>
2024-01-12 20:58:47 -0500

    Basic Columning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
 <!-- HTML. -->
 <main>
-	<h1 class='title'>title</h1>
-	<button class='button is-danger'>button</button>
-	<Bookmarks></Bookmarks>
+	<div class="container p-3 borderify">
+		<div class="container m-1 borderify">
+			<NavBar></NavBar>
+		</div>
+		<div class="columns m-1">
+			<div class="column is-8 borderify">
+				<Bookmarks></Bookmarks>
+			</div>
+			<div class="column is-4 borderify">
+				<Clock></Clock>
+			</div>
+		</div>
+	</div>
 </main>
 
 <!-- CSS. -->
-<style></style>
+<style>
+	.borderify {
+		border: 5px solid red;
+	}
+</style>
 
 <!-- TypeScript. -->
 <script lang='ts'>
 	import Bookmarks from './bookmarks.svelte';
+	import NavBar from './navigation-bar.svelte';
+	import Clock from './clock.svelte';
 </script>
